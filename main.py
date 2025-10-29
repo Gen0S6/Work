d0e5a7b01a6a730c015a5ad16695ffff1257e697
Gen0S6 <chajosst@gmail.com>
2025-10-29 15:03:10 +0100

    ain

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 ﻿print("System boot successful. Welcome to Gen0S6")
-#first commit
-#second commit
+
+for i in range(5):
+    print("Loop number", i)
+
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Steve")
+greet("Lucie")
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+with open("fill.txt","w") as file:
+    file.write("first note.") 
+with open("fill.txt","r") as file:
+    content = file.read()
+    print(content)
